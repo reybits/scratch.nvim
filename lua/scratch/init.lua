b6fb1d842ac95cd1cc7a2f747bb2da40c08bcde4
Andrey A. Ugolnik <andrey@ugolnik.info>
2025-03-16 19:39:47 +0100

    feat: dynamic window sizing for scratch buffer

--- lua/scratch/init.lua
+++ lua/scratch/init.lua
@@ -3,12 +3,17 @@ local M = {}
 M.config = {
 	title = " Scratch ",
 	relative = "editor",
-	width = 60,
-	height = 40,
-	-- row = 5,
-	-- col = 10,
+	width = 1,
+	height = 1,
+	row = 1,
+	col = 1,
 	style = "minimal",
 	border = "rounded",
+}
+
+local win_property = {
+	width = 0.8,
+	height = 0.8,
 }
 
 local function is_buffer_modified()
@@ -20,6 +25,11 @@ local floating_win = nil
 
 M.toggle = function()
 	if floating_win == nil or not vim.api.nvim_win_is_valid(floating_win) then
+		M.config.width = math.floor(win_property.width * vim.o.columns)
+		M.config.height = math.floor(win_property.height * vim.o.lines)
+		M.config.col = math.floor((vim.o.columns - M.config.width) / 2)
+		M.config.row = math.floor((vim.o.lines - M.config.height) / 2)
+
 		-- Create a new floating window if it does not exist
 		local buf = vim.api.nvim_create_buf(false, true) -- create a new empty buffer
 		floating_win = vim.api.nvim_open_win(buf, true, M.config)
@@ -44,9 +54,12 @@ end
 
 function M.setup(opts)
 	opts = opts or {}
+	M.config.title = opts.title or M.config.title
+	win_property.width = opts.width or 0.8
+	win_property.height = opts.height or 0.8
 
 	-- Merge the provided options with the default configuration
-	vim.tbl_deep_extend("force", M.config, opts)
+	-- opts = vim.tbl_deep_extend("force", M.config, opts)
 
 	-- Bind commands to our lua functions
 	vim.api.nvim_create_user_command("ScratchToggle", M.toggle, {})
